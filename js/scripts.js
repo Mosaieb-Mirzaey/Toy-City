9b4f03b8515d4eb02f9483c3dc0fdedba22955c3
Mosaieb-Mirzaey <mosaieb.mirzaey@gmail.com>
2020-09-28 00:43:29 +0330

    slider set up

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,22 @@ navbarBtn.addEventListener("click" , function (){
         navbarList.style.top = "-8.1rem"
     }
 })
+
+
+
+
+let searchBox = document.querySelector("#search-box");
+let search = document.querySelector("header .search");
+// let navbarNav = document.querySelector("#navbarNav");
+
+search.addEventListener("click", function (){
+    if (searchBox.classList.contains("deactivate")){
+        // navbarNav.classList.remove("show")
+        searchBox.classList.remove("deactivate");
+        searchBox.classList.add("activate")
+    }else {
+        searchBox.classList.remove("activate");
+        searchBox.classList.add("deactivate");
+        // navbarNav.classList.remove("show")
+    }
+})

69d40484a6a2891202a0098988de1d290b78555b
Mosaieb Mirzaey <mosaieb.mirzaey@gmail.com>
2020-11-12 14:20:29 +0330

    filter nav mobile

--- js/search.js
+++ js/search.js
@@ -1,4 +1,78 @@
 
+// bootstrap Number Btn
+(function ( $ ) {
+    $.fn.bootstrapNumber = function( options ) {
+
+        var settings = $.extend({
+            upClass: 'default',
+            downClass: 'default',
+            upText: '+',
+            downText: '-',
+            center: true
+        }, options );
+
+        return this.each(function(e) {
+            var self = $(this);
+            var clone = self.clone(true, true);
+
+            var min = self.attr('min');
+            var max = self.attr('max');
+            var step = (parseInt(self.attr('step')) || 1);
+
+            function setText(n) {
+                if (isNaN(n) || (min && n < min) || (max && n > max)) {
+                    return false;
+                }
+
+                clone.focus().val(n);
+                clone.trigger('change');
+                return true;
+            }
+
+            var group = $("<div class='input-group'></div>");
+            var down = $("<button type='button'>" + settings.downText + "</button>").attr('class', 'btnDown btn btn-outline-' + settings.downClass).click(function() {
+                setText(parseInt(clone.val() || clone.attr('value')) - step);
+            });
+            var up = $("<button type='button'>" + settings.upText + "</button>").attr('class', 'btnUp btn btn-outline-' + settings.upClass).click(function() {
+                setText(parseInt(clone.val() || clone.attr('value')) + step);
+            });
+            $("<span class='input-group-btn'></span>").append(down).appendTo(group);
+            clone.appendTo(group);
+            if(clone && settings.center) {
+                clone.css('text-align', 'center');
+            }
+            $("<span class='input-group-btn'></span>").append(up).appendTo(group);
+
+            // remove spins from original
+            clone.prop('type', 'text').keydown(function(e) {
+                if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                    (e.keyCode == 65 && e.ctrlKey === true) ||
+                    (e.keyCode >= 35 && e.keyCode <= 39)) {
+                    return;
+                }
+                if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                    e.preventDefault();
+                }
+
+                var c = String.fromCharCode(e.which);
+                var n = parseInt(clone.val() + c);
+
+                if ((min && n < min) || (max && n > max)) {
+                    e.preventDefault();
+                }
+            });
+
+            self.replaceWith(group);
+        });
+    };
+} ( jQuery ));
+$('#after').bootstrapNumber();
+$('.colorful').bootstrapNumber({
+    upClass: 'success',
+    downClass: 'danger'
+});
+
+
 // Branches Collapse Btn
 function Collapse1(item){
     if (item.lastElementChild.classList.contains("fa-plus")){
@@ -9,7 +83,7 @@ function Collapse1(item){
         $(item).next().children().css({"margin-top":"-840px", "transition":"500ms"})
     }
 }
-
+// SubBranches Collapse Btn
 function categoryCollapse2(item){
     if (item.lastElementChild.classList.contains("fa-plus")){
         $(item).children().last().removeClass("fa-plus").addClass("fa-minus");
@@ -19,6 +93,28 @@ function categoryCollapse2(item){
         $(item).next().children().css({"margin-top":"-100%", "transition":"500ms"})
     }
 }
+
+// Branches Collapse Btn Mobile
+function collapseMobile1(item){
+    if (item.classList.contains("false")){
+        $(item).removeClass("false").addClass("true");
+        $(item).next().css({"height":"auto", "transition":"500ms", })
+    }else {
+        $(item).removeClass("true").addClass("false");
+        $(item).next().css({"height":"0", "transition":"500ms", })
+    }
+}
+// SubBranches Collapse Btn Mobile
+function collapseMobile2(item){
+    if (item.firstElementChild.classList.contains("fa-plus")){
+        $(item).children().first().removeClass("fa-plus").addClass("fa-minus");
+        $(item).next().css({"height":"auto", "transition":"500ms", })
+    }else {
+        $(item).children().first().removeClass("fa-minus").addClass("fa-plus");
+        $(item).next().css({"height":"0", "transition":"500ms", })
+    }
+}
+
 
 
 
